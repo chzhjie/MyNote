e913fe1ceddcbc11ac15ecaee5026bee0e7ce097
zhenjie <chzhjie0827@126.com>
2015-11-05 08:05:52 +0800

    add logUtils class

--- app/src/main/java/com/example/changzhenjie/mynote/util/LogUtils.java
+++ app/src/main/java/com/example/changzhenjie/mynote/util/LogUtils.java
@@ -6,21 +6,95 @@ import android.util.Log;
  * Created by zhenjie on 2015/5/15.
  */
 public class LogUtils {
-    public static boolean ISLOG =   true;
-    public static final String NORMAL_TAG = "LOG";
+    private static final String LOG_PREFIX = "note_";
+    private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
+    private static final int MAX_LOG_TAG_LENGTH = 23;
 
-    public static void info(String s,Object... args){
-        Log.d(NORMAL_TAG,String.format(s, args));
-    }
-    public static void info(String tag,String s,Object... args){
-        Log.d(tag,String.format(s, args));
+    public static boolean LOGGING_ENABLED = true;
+
+    public static String makeLogTag(String str) {
+        if (str.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
+            return LOG_PREFIX + str.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH - 1);
+        }
+
+        return LOG_PREFIX + str;
     }
 
-    public static void d(String tag,String s){
-        Log.d(tag,s);
+    /**
+     * Don't use this when obfuscating class names!
+     */
+    public static String makeLogTag(Class cls) {
+        return makeLogTag(cls.getSimpleName());
     }
 
-    public static void d(String s){
-        Log.d(NORMAL_TAG,s);
+    public static void LOGD(final String tag, String message) {
+        if (LOGGING_ENABLED){
+            if (Log.isLoggable(tag, Log.DEBUG)) {
+                Log.d(tag, message);
+            }
+        }
+    }
+
+    public static void LOGD(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED){
+            if (Log.isLoggable(tag, Log.DEBUG)) {
+                Log.d(tag, message, cause);
+            }
+        }
+    }
+
+    public static void LOGV(final String tag, String message) {
+        if (LOGGING_ENABLED) {
+            if (Log.isLoggable(tag, Log.VERBOSE)) {
+                Log.v(tag, message);
+            }
+        }
+    }
+
+    public static void LOGV(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            if (Log.isLoggable(tag, Log.VERBOSE)) {
+                Log.v(tag, message, cause);
+            }
+        }
+    }
+
+    public static void LOGI(final String tag, String message) {
+        if (LOGGING_ENABLED) {
+            Log.i(tag, message);
+        }
+    }
+
+    public static void LOGI(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            Log.i(tag, message, cause);
+        }
+    }
+
+    public static void LOGW(final String tag, String message) {
+        if (LOGGING_ENABLED) {
+            Log.w(tag, message);
+        }
+    }
+
+    public static void LOGW(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            Log.w(tag, message, cause);
+        }
+    }
+
+    public static void LOGE(final String tag, String message) {
+        if (LOGGING_ENABLED){
+            Log.e(tag, message);
+        }
+    }
+
+    public static void LOGE(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            Log.e(tag, message, cause);
+        }
+    }
+
+    private LogUtils() {
     }
 }
