09c4c028f3bbbeea8a773ddf3a8d610ebdaf1ad7
wei.huang-nb <wei.huang-nb@jrdcom.com>
2015-11-07 17:26:07 +0800

    add some Activitys of the UI

--- app/src/main/java/com/example/changzhenjie/mynote/activity/BaseActivity.java
+++ app/src/main/java/com/example/changzhenjie/mynote/activity/BaseActivity.java
@@ -1,7 +1,10 @@
 package com.example.changzhenjie.mynote.activity;
 
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,23 +12,53 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.example.changzhenjie.mynote.homepage.HomePageActivity;
+import com.example.changzhenjie.mynote.moneymanager.MoneyManager;
+import com.example.changzhenjie.mynote.paymentbalance.PaymentsActivity;
 import com.example.changzhenjie.mynote.R;
-import com.example.changzhenjie.mynote.categories.Categories;
+import com.example.changzhenjie.mynote.categories.CategoriesActivity;
+import com.example.changzhenjie.mynote.util.LogUtils;
 
 /**
  * Created by changzhenjie on 11/5/15.
  */
-public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = LogUtils.makeLogTag(BaseActivity.class);
+
+    public static final int NAVI_DRAWER_ITEM_INVALID = -1;
+
+
+    // fade in and fade out durations for the main content when switching between
+    // different Activities of the app through the Nav Drawer
+    private static final int MAIN_CONTENT_FADEOUT_DURATION = 150;
+
+    private static final int MAIN_CONTENT_FADEIN_DURATION = 250;
+    // delay to launch nav drawer item, to allow close animation to play
+    private static final int NAVDRAWER_LAUNCH_DELAY = 250;
+
+    Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mHandler = new Handler();
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+        View mainContent = findViewById(R.id.main_content);
+        //it is used for switch the activity fade in animation
+//        if(mainContent != null){
+//            mainContent.setAlpha(0);
+//            mainContent.animate().alpha(1).setDuration(MAIN_CONTENT_FADEIN_DURATION);
+//        }else{
+//            LogUtils.LOGD(TAG,"No main content to fade in");
+//        }
     }
 
     @Override
@@ -36,6 +69,11 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
+
+        setupToolBarAndNaviDrawer();
+    }
+
+    private void setupToolBarAndNaviDrawer() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -45,30 +83,78 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(getSelfNaviDrawerItem());
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_payments) {
-
-        } else if (id == R.id.nav_wallet) {
-
-        } else if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_about) {
-
-        } else if (id == R.id.nav_categories){
-            Intent intent = new Intent(BaseActivity.this, Categories.class);
-            startActivity(intent);
+        final int id = item.getItemId();
+        if (getSelfNaviDrawerItem() == id) {
+            // if this page is already the dest page,we don't do anything
+            return true;
         }
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                gotoNaviDrawerItem(id);
+            }
+        },NAVDRAWER_LAUNCH_DELAY);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void gotoNaviDrawerItem(final int id) {
+        if (id == R.id.nav_home) {
+            Intent intent = new Intent(BaseActivity.this, HomePageActivity.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_payments) {
+            Intent intent = new Intent(BaseActivity.this, PaymentsActivity.class);
+            createBackStack(intent);
+        } else if (id == R.id.nav_wallet) {
+            Intent intent = new Intent(BaseActivity.this, MoneyManager.class);
+            createBackStack(intent);
+        } else if (id == R.id.nav_settings) {
+//            Intent intent = new Intent(BaseActivity.this, SettingsActivity.class);
+//            createBackStack(intent);
+        } else if (id == R.id.nav_about) {
+
+        } else if (id == R.id.nav_categories) {
+            Intent intent = new Intent(BaseActivity.this, CategoriesActivity.class);
+            createBackStack(intent);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            if(getSelfNaviDrawerItem() == R.id.nav_home){
+                finish();
+            }
+            super.onBackPressed();
+        }
+    }
+
+    private void createBackStack(Intent intent) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            stackBuilder.addNextIntentWithParentStack(intent);
+            stackBuilder.startActivities();
+        } else {
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    protected int getSelfNaviDrawerItem() {
+        return NAVI_DRAWER_ITEM_INVALID;
     }
 }
---
+++ app/src/main/java/com/example/changzhenjie/mynote/paymentbalance/PaymentsActivity.java
@@ -0,0 +1,58 @@
+package com.example.changzhenjie.mynote.paymentbalance;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.changzhenjie.mynote.R;
+import com.example.changzhenjie.mynote.activity.BaseActivity;
+
+public class PaymentsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payments);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+       overridePendingTransition(0, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.payments, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected int getSelfNaviDrawerItem() {
+        return R.id.nav_payments;
+    }
+}
